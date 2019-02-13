40ce3ce8c1c750a98c4f6ca1765239d4a3bdb648
GucciLee <guccilee@163.com>
2019-02-13 16:42:45 +0800

    add easysms

---
+++ config/easysms.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 一款满足你的多种发送需求的短信发送组件
+ * https://github.com/overtrue/easy-sms
+ */
+return [
+    // HTTP 请求的超时时间(秒)
+    'timeout' => 5.0,
+
+    // 默认发送配置
+    'default'=> [
+        //  网关调用策略, 默认:顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'aliyun',
+        ],
+    ],
+
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'aliyun' => [
+            'access_key_id' => env('ALIYUN_ACCESS_KEY_ID'),
+            'access_key_secret' => env('ALIYUN_ACCESS_KEY_SECRET'),
+            'sign_name' => env('ALIYUN_SIGN_NAME'),
+        ],
+    ],
+];
